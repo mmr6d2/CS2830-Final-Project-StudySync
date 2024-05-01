d37118328444e405ca6022c749054af1d74bda6e
MichaelBai888 <123405247+MichaelBai888@users.noreply.github.com>
2024-04-30 19:57:46 -0500

    Add files via upload

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
+import AuthForm from './AuthForm';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import WeekView from './Pages/WeekView';
-import DayView from './Pages/DayView';
-import MonthView from './Pages/MonthView';
-
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-        <h1 className=" text-blue-500 font-bold py-2 px-4 rounded text-xl">Weekly Planner</h1>
-
-          <div className="header-buttons">
-            <Link to="/day" className="mr-1 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Day</Link>
-            <Link to="/" className="ml-1 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Week</Link>
-            <Link to="/month" className="ml-1 inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Month</Link>
-          </div>
-        </header>
-        <Routes>
-          <Route path="/" element={<WeekView />} />
-          <Route path="/day" element={<DayView />} />
-          <Route path="/month" element={<MonthView />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <AuthForm />
+    </div>
   );
-};
+}
 
 export default App;
